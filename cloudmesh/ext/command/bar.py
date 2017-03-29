3b3def622591064d77093740a0c336f2f029d991
Gregor von Laszewski <laszewski@gmail.com>
2017-03-29 06:10:45 -0400

    fix:usr: fix import to the command decorator

--- cloudmesh/ext/command/bar.py
+++ cloudmesh/ext/command/bar.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
-from cloudmesh_client.shell.command import command
-from cloudmesh_client.shell.command import PluginCommand
+from cloudmesh.shell.command import command
+from cloudmesh.shell.command import PluginCommand
 
 
 class BarCommand(PluginCommand):
